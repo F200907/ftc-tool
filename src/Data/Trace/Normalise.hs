3836339bb05ed5e362a65811b781422c2f64367c
Filip Stevanovic <steva@kth.se>
2025-04-09 12:42:14 +0200

    Normalisation comment

--- src/Data/Trace/Normalise.hs
+++ src/Data/Trace/Normalise.hs
@@ -5,10 +5,13 @@
 module Data.Trace.Normalise (Normalisable (..)) where
 
 -- import Data.Text ()
-import Data.Trace.TraceLogic (TraceFormula (..))
+
 import Data.Trace.Program (Statement (..))
+import Data.Trace.TraceLogic (TraceFormula (..))
 
 class Normalisable a where
+  -- |
+  -- Normalises an inductive structure such that certain binary connectives are nested to the right.
   normalise :: a -> a
 
 instance (Normalisable TraceFormula) where
@@ -32,7 +35,6 @@ instance (Normalisable Statement) where
     s1' -> Sequence s1' (normalise s3)
   normalise (Condition b s1 s2) = Condition b (normalise s1) (normalise s2)
   normalise s@(Method _) = s
-    
 
 -- normaliseCheck :: TraceFormula -> Bool
 -- normaliseCheck (Conjunction tf1 tf2) = normaliseCheck tf1 && normaliseCheck tf2
